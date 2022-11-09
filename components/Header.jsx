ffd82b0e8d31e159796518d967a92306a34f2b51
Kristophercole <kristopher.cole52@gmail.com>
2022-11-09 15:49:25 +0800

    Banner Finished

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import { useState, useEffect } from 'react';
+import { BellIcon, MagnifyingGlassIcon } from '@heroicons/react/24/solid';
 
 const Header = () => {
   const [isScrolled, setIsScrolled] = useState(false);
@@ -40,31 +41,9 @@ const Header = () => {
       </div>
 
       <div className='flex items-center space-x-4 text-sm font-light'>
-        <svg
-          xmlns='http://www.w3.org/2000/svg'
-          viewBox='0 0 24 24'
-          fill='currentColor'
-          className='hidden sm:inline w-6 h-6'
-        >
-          <path
-            fillRule='evenodd'
-            d='M10.5 3.75a6.75 6.75 0 100 13.5 6.75 6.75 0 000-13.5zM2.25 10.5a8.25 8.25 0 1114.59 5.28l4.69 4.69a.75.75 0 11-1.06 1.06l-4.69-4.69A8.25 8.25 0 012.25 10.5z'
-            clipRule='evenodd'
-          />
-        </svg>
+        <MagnifyingGlassIcon className='hidden sm:inline w-6 h-6' />
         <p className='hidden lg:inline'>Kids</p>
-        <svg
-          xmlns='http://www.w3.org/2000/svg'
-          viewBox='0 0 24 24'
-          fill='currentColor'
-          className='w-6 h-6'
-        >
-          <path
-            fillRule='evenodd'
-            d='M5.25 9a6.75 6.75 0 0113.5 0v.75c0 2.123.8 4.057 2.118 5.52a.75.75 0 01-.297 1.206c-1.544.57-3.16.99-4.831 1.243a3.75 3.75 0 11-7.48 0 24.585 24.585 0 01-4.831-1.244.75.75 0 01-.298-1.205A8.217 8.217 0 005.25 9.75V9zm4.502 8.9a2.25 2.25 0 104.496 0 25.057 25.057 0 01-4.496 0z'
-            clipRule='evenodd'
-          />
-        </svg>
+        <BellIcon className='w-6 h-6' />
         <Link href='/account'>
           <img
             src='https://occ-0-1190-2774.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABbme8JMz4rEKFJhtzpOKWFJ_6qX-0y5wwWyYvBhWS0VKFLa289dZ5zvRBggmFVWVPL2AAYE8xevD4jjLZjWumNo.png?r=a41'
---
+++ movieObjects.js
@@ -0,0 +1,33 @@
+// const Genre = {
+//   id: number,
+//   name: '',
+// };
+
+// const Movie = {
+//   title: '',
+//   backdrop_path: '',
+//   media_type: '',
+//   release_date: '',
+//   first_air_date: '',
+//   genre_ids: [],
+//   id: number,
+//   name: '',
+//   origin_country: [],
+//   original_language: '',
+//   original_name: '',
+//   overview: '',
+//   popularity: number,
+//   poster_path: '',
+//   vote_average: number,
+//   vote_count: number,
+// };
+
+// const Element = {
+//   type:
+//     || 'Bloopers'
+//     || 'Featurette'
+//     || 'Behind the Scenes'
+//     || 'Clip'
+//     || 'Trailer'
+//     || 'Teaser'
+// }
